99fc085d0a70a000f0e58cd556ae09b145676f82
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-24 22:06:03 +0800

    create category model

---
+++ server/models/category.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Name is required",
+        minlength: [2, "too short"],
+        maxlength: [32, "too long"],
+    }, slug: {
+        type: String, unique: true, lowercase: true, index: true
+    }, state: {
+        type: Number, default: 1
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("Category", categorySchema)

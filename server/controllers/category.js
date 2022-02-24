49b80aaf767558d0a04d6eb9637f0920e6489546
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-24 23:24:04 +0800

    create category crud

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,17 +1,45 @@
 const Category = require('../models/category')
-
-exports.create = (req, res) => {
-
+const slugify = require('slugify')
+exports.create = async (req, res) => {
+    try {
+        const {name} = req.body
+        const category = await new Category({name, slug: slugify(name)}).save()
+        res.json(category)
+    } catch (e) {
+        res.status(400).json(e)
+    }
 }
-exports.list = (req, res) => {
-
+exports.list = async (req, res) => {
+    try {
+        const category = await Category.find({}).sort({createdAt: -1}).exec()
+        res.json(category)
+    } catch (e) {
+        res.status(400).json(e)
+    }
 }
-exports.read = (req, res) => {
-
+exports.read = async (req, res) => {
+    try {
+        const slug = req.params.slug
+        const category = await Category.findOne({slug}).exec()
+        res.json(category)
+    } catch (e) {
+        res.status(400).json(e)
+    }
 }
-exports.update = (req, res) => {
-
+exports.update = async (req, res) => {
+    try {
+        const {name} = req.body
+        const category = Category.findByIdAndUpdate({slug: req.params.slug}, {name, slug: slugify(name)})
+        res.json(category)
+    } catch (e) {
+        res.status().json(e)
+    }
 }
-exports.remove = (req, res) => {
-
+exports.remove = async (req, res) => {
+    try {
+        const category = await Category.findOneAndDelete({slug: req.params.slug})
+        res.json(category)
+    } catch (e) {
+        res.status().json(e)
+    }
 }

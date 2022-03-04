0ffa6af914f641c65f1165032a294ebbc5eb3d04
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-04 12:22:09 +0800

    Charging Actual Cart Total

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -4,9 +4,11 @@ const Cart = require("../models/cart");
 const Product = require("../models/products");
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KET)
 exports.createPaymentIntent = async (req, res) => {
+    const user = await User.findOne({email: req.user.email}).exec()
+    const {cartTotal} = await Cart.findOne({orderedBy: user._id}).exec()
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 500,
-        currency: "usd",
+        amount: cartTotal * 100,
+        currency: "hkd",
     })
     res.send({
         clientSecret: paymentIntent.client_secret
